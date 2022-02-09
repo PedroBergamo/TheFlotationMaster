c39a9e3cf49afa82940ba7398edc4a532782ab2f
Pedro Bergamo <pedrobergamo89@gmail.com>
2022-02-09 11:32:01 +0200

    Fixing froth unit and air influence

--- Assets/Scripts/Variables/FrothThicknessControl.cs
+++ Assets/Scripts/Variables/FrothThicknessControl.cs
@@ -3,9 +3,10 @@
 public class FrothThicknessControl : MonoBehaviour {
     public PID FrothPID;
     public FlotationCalculation simulation;
+    public double didacticFactor = 5;
 
     void Update()
     {
-        simulation.Simulation.FrothHeight = FrothPID.ProcessValue / 100;
+        simulation.Simulation.FrothHeight = FrothPID.ProcessValue / 100 * didacticFactor;
     }
 }
